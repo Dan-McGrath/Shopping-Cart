7edeef0359eb6ba77ef03795627e34f4828c142c
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-09 17:20:32 -0400

    Create basic nav component

---
+++ shopping-cart/src/components/header/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+        <nav>
+            <img src="" alt="logo" />
+            <ul>
+                <Link>Home</Link>
+                <Link>Shopping</Link>
+                <Link>Cart</Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
