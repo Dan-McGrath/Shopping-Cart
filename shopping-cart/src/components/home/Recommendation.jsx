5c4131df0140691e0c1f2e590e92906afd270fae
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-09 19:27:12 -0400

    Add carousel for recommended products

---
+++ shopping-cart/src/components/home/Recommendation.jsx
@@ -0,0 +1,48 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { useState, useEffect } from "react";
+import Card from "../Card";
+
+const Recommendation = () => {
+  const [productArray, setProductArray] = useState([]);
+  const settings = {
+    infiniteLoop: true,
+    autoPlay: true,
+    stopOnHover: true,
+    swipeable: true,
+    showThumbs: false,
+  };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products?limit=7");
+      const data = await response.json();
+      setProductArray(data);
+    };
+    fetchProducts();
+  }, []);
+
+  const products = productArray.map((ele) => (
+    <Card
+      key={ele.id}
+      title={ele.title}
+      price={ele.price}
+      category={ele.category}
+      description={ele.description}
+      image={ele.image}
+    />
+  ));
+
+  return (
+    <>
+      <section className="recommendations">
+        <h2>Recommendations</h2>
+        <div className="wrapper">
+          <Carousel {...settings}>{products}</Carousel>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Recommendation;
