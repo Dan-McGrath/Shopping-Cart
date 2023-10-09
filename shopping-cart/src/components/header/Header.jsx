6c55319594081556353222f0b8c7a59d32857b68
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-09 17:20:57 -0400

    create header compnent

---
+++ shopping-cart/src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import Nav from "./Nav";
+
+const Header = ({ isMobile, navHandler, clickLinkHandler, navIsOpen }) => {
+  return isMobile ? (
+    navIsOpen ? (
+      <header className="nav-mobile">
+        <div className="hamburger">
+          <div className="close-icon" onClick={navHandler}>
+            <p className="sr-only">Close</p>
+            <i className="fa-solid fa-x"></i>
+          </div>
+        </div>
+        <Nav clickLinkHandler={clickLinkHandler} />
+      </header>
+    ) : (
+      <header>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="hamburger">
+          <div className="open-icon" onClick={navHandler}>
+            <p className="sr-only">Menu</p>
+            <i className="fa-solid fa-bars"></i>
+          </div>
+        </div>
+      </header>
+    )
+  ) : (
+    <header>
+      <img src={logo} alt="logo" />
+      <Nav />
+    </header>
+  );
+};
+
+Header.propTypes = {
+  isMobile: PropTypes.bool,
+  navHandler: PropTypes.func,
+  clickLinkHandler: PropTypes.func,
+  navIsOpen: PropTypes.bool,
+};
+
+export default Header;
