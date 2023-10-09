f042481b90526b0948aae4a3496a97908c3b2131
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-09 19:26:45 -0400

    Create card to hold product info

---
+++ shopping-cart/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const Card = ({ title, price, category, description, image }) => {
+  return (
+    <div className="item">
+      <h3>{title}</h3>
+      <img src={image} alt={title} />
+      <div className="info">
+        <p className="description">{description}</p>
+        <p className="price">{price}</p>
+        <p className="category">{category}</p>
+      </div>
+      <div className="buttons"></div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string,
+  price: PropTypes.number,
+  category: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+};
+
+export default Card;
