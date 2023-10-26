11c89e4119c9c57288dcd322717c07021637cb1d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-25 21:32:57 -0400

    add status message

--- shopping-cart/src/components/ErrorPage.jsx
+++ shopping-cart/src/components/ErrorPage.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
   return (
     <div>
       <h1>Oh no, this route doesn&apos;t exist!</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
       </Link>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
     </div>
   );
 };
