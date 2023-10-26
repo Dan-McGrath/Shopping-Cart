bea99f7d7a03342a95a68c344e8d5626e7df07e4
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-25 22:23:41 -0400

    create basic handlers for adding/removing from cart

--- shopping-cart/src/components/Card.jsx
+++ shopping-cart/src/components/Card.jsx
@@ -1,33 +1,55 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import { Form, Link } from "react-router-dom";
 
-const Card = ({ title, price, category, description, image }) => {
+const Card = ({ id, title, price, category, description, image }) => {
   const [quantity, setQuantity] = useState(1);
+  const [inCart, setInCart] = useState(false);
 
   category = category.charAt(0).toUpperCase() + category.slice(1);
+
+  const addToCartHandler = (e) => {
+    e.preventDefault();
+    setInCart(true);
+  };
+
+  const removeFromCartHandler = (e) => {
+    e.preventDefault();
+    setInCart(false);
+  };
+
   return (
-    <div className="item" style={{ margin: "2em auto" }}>
-      <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <div className="info">
-        <p className="description">{description}</p>
-        <p className="price">${price}</p>
-        <p className="category">{category}</p>
+    <Link to={`product/${id}`}>
+      <div className="item" style={{ margin: "2em auto" }}>
+        <h3>{title}</h3>
+        <img src={image} alt={title} />
+        <div className="info">
+          <p className="price">${price}</p>
+          <p className="category">{category}</p>
+        </div>
+        <div className="product-form">
+          <Form>
+            {inCart ? (
+              <button onClick={removeFromCartHandler}>Remove from Cart</button>
+            ) : (
+              <button onClick={addToCartHandler}>Add to Cart</button>
+            )}
+          </Form>
+          <Form>
+            <label htmlFor="quantity">Quantity</label>
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              min="1"
+              max="99"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </Form>
+        </div>
       </div>
-      <div className="buttons">
-        <button>Add to Cart</button>
-        <label htmlFor="quantity">Quantity</label>
-        <input
-          type="number"
-          id="quantity"
-          name="quantity"
-          min="1"
-          max="99"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
-      </div>
-    </div>
+    </Link>
   );
 };
 
@@ -37,6 +59,7 @@ Card.propTypes = {
   category: PropTypes.string,
   description: PropTypes.string,
   image: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export default Card;
