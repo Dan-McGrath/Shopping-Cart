6627e159bf37ff9dd7aad3debb7985e0a78273e9
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-31 21:53:33 -0400

    add loader for game

--- shopping-cart/src/components/Game.jsx
+++ shopping-cart/src/components/Game.jsx
@@ -1,8 +1,15 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { Form, Link } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { fetchGame } from "../apiHelpers/games";
 
-const Game = ({ slug, title, rating, genre, image }) => {
+export const loader = async ({ params }) => {
+  const game = await fetchGame(params.gameId);
+  return { game };
+};
+
+const Game = () => {
+  const { game } = useLoaderData();
   const [quantity, setQuantity] = useState(1);
   const [inCart, setInCart] = useState(false);
 
@@ -17,47 +24,52 @@ const Game = ({ slug, title, rating, genre, image }) => {
   };
 
   return (
-    <Link to={`product/${slug}`}>
-      <div className="item" style={{ margin: "2em auto" }}>
-        <h3>{title}</h3>
-        <img src={image} alt={title} />
-        <div className="info">
-          <p className="price">Rating: {rating}</p>
-          <p className="category">{genre}</p>
+    <div className="game" style={{ margin: "2em auto" }}>
+      {game.map((ele) => (
+        <div key={ele.id}>
+          <h3>{ele.name}</h3>
+          <img src={ele.background_image} alt={ele.title} />
+          <div className="info">
+            <p className="rating">Rating: {ele.rating}</p>
+            <p className="released">Released: {ele.released}</p>
+          </div>
+          <div className="product-form">
+            <Form>
+              {inCart ? (
+                <button onClick={removeFromCartHandler}>
+                  Remove from Cart
+                </button>
+              ) : (
+                <button onClick={addToCartHandler}>Add to Cart</button>
+              )}
+            </Form>
+            <Form>
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="1"
+                max="99"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            </Form>
+          </div>
         </div>
-        <div className="product-form">
-          <Form>
-            {inCart ? (
-              <button onClick={removeFromCartHandler}>Remove from Cart</button>
-            ) : (
-              <button onClick={addToCartHandler}>Add to Cart</button>
-            )}
-          </Form>
-          <Form>
-            <label htmlFor="quantity">Quantity</label>
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              min="1"
-              max="99"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-          </Form>
-        </div>
-      </div>
-    </Link>
+      ))}
+    </div>
   );
 };
 
 Game.propTypes = {
   title: PropTypes.string,
   rating: PropTypes.number,
-  genre: PropTypes.string,
+  genre: PropTypes.array,
   description: PropTypes.string,
   image: PropTypes.string,
-  slug: PropTypes.number,
+  slug: PropTypes.string,
+  released: PropTypes.string,
 };
 
 export default Game;
