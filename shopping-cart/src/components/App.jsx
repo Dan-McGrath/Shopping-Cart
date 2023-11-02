f5ac04c734dec47394915e7b8f2c73834f382843
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-01 21:45:42 -0400

    simplify component

--- shopping-cart/src/components/App.jsx
+++ shopping-cart/src/components/App.jsx
@@ -1,15 +1,8 @@
-import { Outlet, useLoaderData } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from "./header/Header";
-import { fetchGenres } from "../apiHelpers/games";
-
-export const loader = async () => {
-  const genres = await fetchGenres();
-  return { genres };
-};
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  const { genres } = useLoaderData();
   const [navIsOpen, setNavIsOpen] = useState(false);
   const [windowDimension, setWindowDimension] = useState(null);
 
@@ -44,13 +37,6 @@ const App = () => {
         clickLinkHandle={clickLinkHandler}
         navIsOpen={navIsOpen}
       />
-      <div className="secondary-nav">
-        <ul>
-          {genres.map((genre) => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
-      </div>
       <Outlet />
     </>
   );
