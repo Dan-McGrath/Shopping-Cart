926f0d82745264590d0ee4bcbbc129ab889b8066
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-02 17:41:28 -0400

    create loader to get cart items

--- shopping-cart/src/components/cart/Cart.jsx
+++ shopping-cart/src/components/cart/Cart.jsx
@@ -1,5 +1,37 @@
+import PropTypes from "prop-types";
+import { getCurrentCart } from "../../helpers/cart.js";
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  const currentCart = await getCurrentCart();
+  return { currentCart };
+};
+
 const Cart = () => {
-  return <h1>Cart</h1>;
+  const { currentCart } = useLoaderData();
+  return (
+    <>
+      <h1>Cart</h1>
+      <ul>
+        {currentCart.length === 0 ? (
+          <p>No games in cart</p>
+        ) : (
+          currentCart.map((game) => (
+            <>
+              <li key={game.id}>
+                <h2>{game.name}</h2>
+                <img src={game.image} alt={game.name} />
+              </li>
+            </>
+          ))
+        )}
+      </ul>
+    </>
+  );
+};
+
+Cart.propTypes = {
+  cart: PropTypes.array,
 };
 
 export default Cart;
