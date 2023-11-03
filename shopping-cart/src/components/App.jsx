aa755fef427a728923456864688fd7a75a708035
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-03 19:45:03 -0400

    create CartContext

--- shopping-cart/src/components/App.jsx
+++ shopping-cart/src/components/App.jsx
@@ -1,10 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import Header from "./header/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
+import { fetchGames } from "../helpers/api";
+
+export const CartContext = createContext({
+  products: [],
+  cartItems: [],
+  addToCart: () => {},
+  removeFromCart: () => {},
+});
+
+export const loader = async () => {
+  const games = await fetchGames();
+  return { games };
+};
 
 const App = () => {
   const [navIsOpen, setNavIsOpen] = useState(false);
   const [windowDimension, setWindowDimension] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const { games } = useLoaderData();
+
+  const products = games;
+
+  const addToCart = (game) => {
+    setCartItems([...cartItems, game]);
+  };
+
+  const removeFromCart = (game) => {
+    let index = cartItems.indexOf(game);
+    let newCart = cartItems.splice(index, 1);
+    setCartItems(newCart);
+  };
 
   useEffect(() => {
     setWindowDimension(window.innerWidth);
@@ -31,13 +58,17 @@ const App = () => {
 
   return (
     <>
-      <Header
-        isMobile={isMobile}
-        navHandler={navHandler}
-        clickLinkHandle={clickLinkHandler}
-        navIsOpen={navIsOpen}
-      />
-      <Outlet />
+      <CartContext.Provider
+        value={{ cartItems, products, addToCart, removeFromCart }}
+      >
+        <Header
+          isMobile={isMobile}
+          navHandler={navHandler}
+          clickLinkHandle={clickLinkHandler}
+          navIsOpen={navIsOpen}
+        />
+        <Outlet />
+      </CartContext.Provider>
     </>
   );
 };
