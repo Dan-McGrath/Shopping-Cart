40a3e1719d6416f53edbdb46facb672c5791fed9
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-03 20:45:17 -0400

    Change text

--- shopping-cart/src/components/home/Home.jsx
+++ shopping-cart/src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <section className="hero">
-      <h1>Welcome To Danify</h1>
+      <h1>Welcome To Arcan Gaming</h1>
       <p>Search for games of all types for purchase!</p>
       <p>
         This is not a real site. Only a demo for what a real game shop would
