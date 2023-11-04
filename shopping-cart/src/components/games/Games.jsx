63e88b2ac75a6a1e9d2476755dfb326ba8d795a0
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-04 19:48:42 -0400

    update styling

--- shopping-cart/src/components/games/Games.jsx
+++ shopping-cart/src/components/games/Games.jsx
@@ -15,6 +15,8 @@ const Games = () => {
   const gameImageStyles = {
     maxWidth: "300px",
     maxHeight: "500px",
+    borderRadius: "10px",
+    margin: "1rem",
   };
 
   const gameTagsStyles = {
@@ -23,17 +25,38 @@ const Games = () => {
     flexWrap: "wrap",
   };
 
+  const gameStyles = {
+    background: "#262626",
+    padding: "1rem",
+    textAlign: "center",
+    borderRadius: "10px",
+    color: "#D923BA",
+  };
+
+  const gameTitleStyles = {
+    fontSize: "20px",
+    margin: "1rem",
+  };
+
   return (
     <>
-      <h1 style={{ textAlign: "center", margin: "2rem", fontSize: "2rem" }}>
+      <h1
+        style={{
+          textAlign: "center",
+          margin: "2rem",
+          fontSize: "2rem",
+          fontWeight: "bold",
+          color: "#D7D7D9",
+        }}
+      >
         Games
       </h1>
       <div className="games" style={gamesStyles}>
         {products.map((game) => (
           <>
-            <div className="game" key={game.id}>
+            <div className="game" key={game.id} style={gameStyles}>
               <Link to={`game/${game.id}`}>
-                <h2>{game.name}</h2>
+                <h2 style={gameTitleStyles}>{game.name}</h2>
                 <img
                   src={game.background_image}
                   alt={game.name}
