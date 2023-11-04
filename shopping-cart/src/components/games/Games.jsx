fc04179fb7fb924146c66ac6b890c1b98ce659fb
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-03 22:21:51 -0400

    add styles

--- shopping-cart/src/components/games/Games.jsx
+++ shopping-cart/src/components/games/Games.jsx
@@ -4,17 +4,42 @@ import { CartContext } from "../App";
 
 const Games = () => {
   const { products } = useContext(CartContext);
+
+  const gamesStyles = {
+    display: "grid",
+    gap: "1rem",
+    gridTemplateColumns: "1fr 1fr 1fr 1fr",
+    maxWidth: "100vw",
+  };
+
+  const gameImageStyles = {
+    maxWidth: "300px",
+    maxHeight: "500px",
+  };
+
+  const gameTagsStyles = {
+    display: "flex",
+    gap: "0.5rem",
+    flexWrap: "wrap",
+  };
+
   return (
     <>
-      <h1>Games</h1>
-      <div className="games">
+      <h1 style={{ textAlign: "center", margin: "2rem", fontSize: "2rem" }}>
+        Games
+      </h1>
+      <div className="games" style={gamesStyles}>
         {products.map((game) => (
           <>
             <div className="game" key={game.id}>
               <Link to={`game/${game.id}`}>
                 <h2>{game.name}</h2>
-                <img src={game.background_image} alt={game.name} />
-                <div className="game-info">
+                <img
+                  src={game.background_image}
+                  alt={game.name}
+                  style={gameImageStyles}
+                />
+                <div className="game-info" style={gameTagsStyles}>
                   {game.tags.map((tag) => (
                     <div className="tag" key={tag.id}>
                       <p>{tag.name}</p>
