82a9e299e2959ecb67afe115319e4a68c1df3e17
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-04 18:03:21 -0400

    add styling

--- shopping-cart/src/components/home/Home.jsx
+++ shopping-cart/src/components/home/Home.jsx
@@ -1,9 +1,23 @@
 const Home = () => {
+  const styles = {
+    maxWidth: "500px",
+    textAlign: "center",
+    margin: "3rem auto",
+    background: "grey",
+    padding: "2rem",
+    border: "1px solid grey",
+    borderRadius: "10px",
+  };
+
   return (
-    <section className="hero">
-      <h1>Welcome To Arcan Gaming</h1>
-      <p>Search for games of all types for purchase!</p>
-      <p>
+    <section className="hero" style={styles}>
+      <h1 style={{ margin: "1rem", fontSize: "36px" }}>
+        Welcome To Arcan Gaming
+      </h1>
+      <p style={{ fontSize: "20px" }}>
+        Search for games of all types for purchase!
+      </p>
+      <p style={{ fontSize: "20px" }}>
         This is not a real site. Only a demo for what a real game shop would
         look like
       </p>
