3507c7542dc6fe0560372ac414ad2df0d12bee67
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-06 00:31:29 -0500

    create styled components

---
+++ shopping-cart/src/components/home/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  max-width: 500px;
+  text-align: center;
+  margin: 3rem auto;
+  background: #262626;
+  padding: 2rem;
+  border: 1px solid #553b8c;
+  border-radius: 10px;
+`;
+
+const Title = styled.h1`
+  margin: 1rem;
+  font-size: 36px;
+  color: #0bb3d9;
+`;
+
+const Text = styled.p`
+  font-size: 20px;
+  color: #d923ba;
+`;
+
+export { Wrapper, Title, Text };
