8767e10c9e2af2b4ef099592fbc6ca783c66796b
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-06 00:30:47 -0500

    add styled components

--- shopping-cart/src/components/home/Home.jsx
+++ shopping-cart/src/components/home/Home.jsx
@@ -1,28 +1,15 @@
-const Home = () => {
-  const styles = {
-    maxWidth: "500px",
-    textAlign: "center",
-    margin: "3rem auto",
-    background: "#262626",
-    padding: "2rem",
-    border: "1px solid #553B8C",
-    borderRadius: "10px",
-    color: "#D923BA",
-  };
+import { Wrapper, Title, Text } from "./styles";
 
+const Home = () => {
   return (
-    <section className="hero" style={styles}>
-      <h1 style={{ margin: "1rem", fontSize: "36px" }}>
-        Welcome To Arcan Gaming
-      </h1>
-      <p style={{ fontSize: "20px" }}>
-        Search for games of all types for purchase!
-      </p>
-      <p style={{ fontSize: "20px" }}>
+    <Wrapper>
+      <Title>Welcome To Arcan Gaming</Title>
+      <Text>Search for games of all types for purchase!</Text>
+      <Text>
         This is not a real site. Only a demo for what a real game shop would
         look like
-      </p>
-    </section>
+      </Text>
+    </Wrapper>
   );
 };
 
