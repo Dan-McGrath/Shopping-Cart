8812846a56088c7055de4e702ff1bc854410e06e
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-06 00:31:22 -0500

    create styled components

---
+++ shopping-cart/src/components/header/navStyles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Navigation = styled.nav`
+  background: #262626;
+  font-size: 20px;
+  color: #0bb3d9;
+  height: 80px;
+  width: 300px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+  gap: 1rem;
+  margin: 1rem;
+`;
+
+export { Navigation, List };
