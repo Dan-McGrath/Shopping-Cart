3e0e46dd9ea608c59eeaed6ecc41aa5aa64d1405
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-06 17:21:03 -0500

    update styled components

--- shopping-cart/src/components/cart/styles.js
+++ shopping-cart/src/components/cart/styles.js
@@ -25,10 +25,67 @@ const List = styled.ul`
 
 const ListItems = styled.li``;
 
+const GameTitle = styled.h2`
+  margin: 1rem;
+  font-size: 1.8rem;
+`;
+
+const GamePrice = styled.p``;
+
+const RemoveButton = styled.button`
+  width: 100%;
+  padding: 0.75rem;
+  border-radius: 20px;
+  border: none;
+  background: #553b8c;
+  margin: 1rem 0;
+  text-aling: center;
+  color: #13f2dc;
+  font-size: 1.6rem;
+  transition: all ease-in-out 0.4s;
+  max-width: 90%;
+
+  &:hover {
+    color: #0bb3d9;
+    background: #d923ba;
+  }
+`;
+
+const GameWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const GameImg = styled.img`
+  max-width: 500px;
+`;
+
+const GameRightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const Quantity = styled.p``;
+
 const Total = styled.div`
   text-align: right;
   margin-right: 2rem;
   color: #d923ba;
+  font-size: 2rem;
 `;
 
-export { Title, List, ListItems, CartWrapper, Total };
+export {
+  GameWrapper,
+  GameImg,
+  GameTitle,
+  Title,
+  List,
+  ListItems,
+  CartWrapper,
+  Total,
+  GamePrice,
+  GameRightWrapper,
+  RemoveButton,
+  Quantity,
+};
