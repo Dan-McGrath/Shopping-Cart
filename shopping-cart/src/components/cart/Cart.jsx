72457c322a39ecdc26ae6755c47c5b361c4377a0
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-05 22:15:03 -0500

    add styled components

--- shopping-cart/src/components/cart/Cart.jsx
+++ shopping-cart/src/components/cart/Cart.jsx
@@ -1,37 +1,39 @@
 import PropTypes from "prop-types";
 import { useContext } from "react";
 import { CartContext } from "../App.jsx";
+import { Title, List, ListItems, CartWrapper, Total } from "./styles.js";
 
 const Cart = () => {
   const { cartItems, removeFromCart, total } = useContext(CartContext);
 
   return (
-    <>
-      <h1>Cart</h1>
-      <ul>
+    <CartWrapper>
+      <Title>Cart</Title>
+      <List>
         {cartItems.length === 0 ? (
           <p>No games in cart</p>
         ) : (
           cartItems.map((game) => (
             <>
-              <li key={game.id}>
+              <ListItems key={game.id}>
                 <h2>{game.name}</h2>
                 <img src={game.background_image} alt={game.name} />
                 <p>Price: ${game.price}</p>
-              </li>
-              <button
-                className="remove-game"
-                type="button"
-                onClick={() => removeFromCart(game)}
-              >
-                Remove from Cart
-              </button>
+
+                <button
+                  className="remove-game"
+                  type="button"
+                  onClick={() => removeFromCart(game)}
+                >
+                  Remove from Cart
+                </button>
+              </ListItems>
             </>
           ))
         )}
-      </ul>
-      <div className="total">Total: ${total}</div>
-    </>
+      </List>
+      <Total>Total: ${total}</Total>
+    </CartWrapper>
   );
 };
 
