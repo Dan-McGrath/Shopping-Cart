6114e89f965e4730a75357a149d585f0243e3ee8
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-07 02:27:45 -0500

    style logo

--- shopping-cart/src/components/header/headerStyles.js
+++ shopping-cart/src/components/header/headerStyles.js
@@ -5,4 +5,13 @@ const MainHeader = styled.header`
   justify-content: space-between;
 `;
 
-export { MainHeader };
+const Logo = styled.img`
+  max-width: 125px;
+  max-height: 125px;
+  border-radius: 500px;
+  background: #262626;
+  margin-top: 1em;
+  margin-left: 1em;
+`;
+
+export { MainHeader, Logo };
