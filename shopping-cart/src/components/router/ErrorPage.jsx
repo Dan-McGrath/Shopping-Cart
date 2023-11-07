d64b3e59e5001218a378f3b46903c390e7562e34
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-07 02:01:55 -0500

    add styled components

--- shopping-cart/src/components/router/ErrorPage.jsx
+++ shopping-cart/src/components/router/ErrorPage.jsx
@@ -1,17 +1,19 @@
 import { Link, useRouteError } from "react-router-dom";
-
+import { Wrapper, ErrorMessage, LinkWrapper } from "./styles";
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div>
+    <Wrapper>
       <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
-      <p>
+      <LinkWrapper>
+        <Link to="/">
+          You can go back to the home page by clicking here, though!
+        </Link>
+      </LinkWrapper>
+      <ErrorMessage>
         <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+      </ErrorMessage>
+    </Wrapper>
   );
 };
 
